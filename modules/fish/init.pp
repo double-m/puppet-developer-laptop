261c70c38b392d5e19ddeb16700d827bf05ed02d
Bruno Bieth <bruno.bieth@nespresso.com>
2014-08-08 09:24:32 +0200

    add fish module

---
+++ modules/fish/init.pp
@@ -0,0 +1,15 @@
+
+class fish {
+
+  apt::ppa { 'ppa:fish-shell/release-2': }
+  ->
+  package { 'fish' :
+    ensure => 'installed'
+  }
+  ->
+  file_line { 'fish alt dot':
+    ensure => present,
+    line => 'bind \e. history-token-search-backward',
+    path => '/usr/share/fish/functions/fish_default_key_bindings.fish',    
+  }
+}
