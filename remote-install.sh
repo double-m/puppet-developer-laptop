1a467a67cf6c6e257e73cfd04c35bdab013f3255
Bruno Bieth <bruno.bieth@nespresso.com>
2014-08-08 09:55:05 +0200

    remote-install

--- remote-install.sh
+++ remote-install.sh
@@ -1,11 +1,17 @@
+set -e
+
 if [ -f master.zip ]; then
   rm -f master.zip
 fi
-if [ -f puppet-developer-laptop-master ]; then
+if [ -d puppet-developer-laptop-master ]; then
   rm -fR puppet-developer-laptop-master
 fi
+
 wget https://github.com/backuitist/puppet-developer-laptop/archive/master.zip
 unzip master.zip
-rm master.zip
+rm -f master.zip
 cd puppet-developer-laptop-master
+
 ./install.sh
+
+rm -fR puppet-developer-laptop-master
