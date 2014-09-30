864b4f728f928b7e143e6dc3a7bf4ada58f8c98a
Bruno Bieth <bruno.bieth@nespresso.com>
2014-09-30 10:48:24 +0200

    mount /tmp with tmpfs

--- init.pp
+++ init.pp
@@ -7,3 +7,12 @@ include 'conky'
 package { ['htop', 'vim'] : 
   ensure => installed,
 }
+
+# /tmp is generally not mounted on tmpfs and will incur IO overhead for tmp operations.
+# Most laptop computers have enough memory to benefit from this
+mount { '/tmp' :
+  ensure      => 'mounted',
+  atboot      => true,
+  fstype      => 'tmpfs',
+  options     => 'defaults,noatime,size=10%,mode=1777'
+}
