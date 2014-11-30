563091fc1a216393c7926510e68c07c2873e2558
Bruno Bieth <biethb@gmail.com>
2014-11-30 19:42:48 +0100

    remove erroneous blank line in update-repo script

--- modules/fish/files/config.fish
+++ modules/fish/files/config.fish
@@ -8,7 +8,6 @@ set -x PATH $PATH:/home/bruno/bin
 function update-repo --description 'Update a single APT repository'
    for source in $argv
       sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/$source" \
-
          -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
    end
 end
