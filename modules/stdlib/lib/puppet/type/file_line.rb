c0139f41d17d414021d1324d6cd2b47d38734e24
Bruno Bieth <bruno.bieth@nespresso.com>
2014-12-29 17:49:06 +0100

    upgrade stdlib to 4.5.0 (remove tests and specs)

--- modules/stdlib/lib/puppet/type/file_line.rb
+++ modules/stdlib/lib/puppet/type/file_line.rb
@@ -21,30 +21,50 @@ Puppet::Type.newtype(:file_line) do
     In this example, Puppet will ensure both of the specified lines are
     contained in the file /etc/sudoers.
 
+    **Autorequires:** If Puppet is managing the file that will contain the line
+    being managed, the file_line resource will autorequire that file.
+
   EOT
 
   ensurable do
+    defaultvalues
     defaultto :present
-    newvalue(:present) do
-      provider.create
-    end
   end
 
   newparam(:name, :namevar => true) do
-    desc 'arbitrary name used as identity'
+    desc 'An arbitrary name used as the identity of the resource.'
+  end
+
+  newparam(:match) do
+    desc 'An optional regular expression to run against existing lines in the file;\n' +
+        'if a match is found, we replace that line rather than adding a new line.'
+  end
+
+  newparam(:multiple) do
+    desc 'An optional value to determine if match can change multiple lines.'
+    newvalues(true, false)
+  end
+
+  newparam(:after) do
+    desc 'An optional value used to specify the line after which we will add any new lines. (Existing lines are added in place)'
   end
 
   newparam(:line) do
-    desc 'The line to be appended to the path.'
+    desc 'The line to be appended to the file located by the path parameter.'
   end
 
   newparam(:path) do
-    desc 'File to possibly append a line to.'
+    desc 'The file Puppet will ensure contains the line specified by the line parameter.'
     validate do |value|
       unless (Puppet.features.posix? and value =~ /^\//) or (Puppet.features.microsoft_windows? and (value =~ /^.:\// or value =~ /^\/\/[^\/]+\/[^\/]+/))
         raise(Puppet::Error, "File paths must be fully qualified, not '#{value}'")
       end
     end
+  end
+
+  # Autorequire the file resource if it's being managed
+  autorequire(:file) do
+    self[:path]
   end
 
   validate do
