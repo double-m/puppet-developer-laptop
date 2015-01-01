513a3c22047f155acebdf4e3b734d89ed603586e
Bruno Bieth <biethb@gmail.com>
2015-01-01 13:11:32 +0100

    do not download scala-2.11.4 if it's already installed

--- modules/scala/manifests/init.pp
+++ modules/scala/manifests/init.pp
@@ -8,14 +8,18 @@ class scala {
     group  => $real_id
   }
 
+  $scala_dir = "${home}/bin/scala-2.11.4"  
+  $scala_dir_exists = "/usr/bin/test -d ${scala_dir}"
+
   # for now we'll only install a single version of scala
   wget::fetch { 'http://downloads.typesafe.com/scala/2.11.4/scala-2.11.4.tgz':
-    destination => "/tmp/scala-2.11.4.tgz",    
+    destination => "/tmp/scala-2.11.4.tgz",   
+    unless      => $scala_dir_exists
   }
   ->
   exec { "/bin/tar -xzf /tmp/scala-2.11.4.tgz -C ${home}/bin/":
-    unless => "/bin/ls ${home}/bin/scala-2.11.4",
-    user  => $real_id,
+    unless => $scala_dir_exists, 
+    user   => $real_id,
     group  => $real_id
   }
   ->
