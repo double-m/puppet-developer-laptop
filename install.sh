f3f3fe4850758d097879815d42275e6db1963f32
Bruno Bieth <biethb@gmail.com>
2015-01-01 13:11:54 +0100

    re-use already installed customization

--- install.sh
+++ install.sh
@@ -2,26 +2,31 @@
 
 set -e
 
-printf "\nEnter the location of your custom module (ssh of file path) or nothing if you have none\n> "
-read custom_location
-
-if [ -z "$custom_location" ]; then
-  echo "No customization enabled"
-  PUPPET_TARGET='all'  
-elif [[ $custom_location == ssh://* ]]; then
-  echo "Installing remote custom module..."
-  set -x
-  mkdir -p modules/custom
-  scp -r ${custom_location:6}/* modules/
-  PUPPET_TARGET='custom'  
+if [ -f modules/custom/manifests/init.pp ]; then
+  PUPPET_TARGET='custom'
 else
-  echo "Installing local custom module..."
-  set -x
-  mkdir -p modules/custom
-  eval EXPANDED_LOCATION=$custom_location/*
-  cp -r $EXPANDED_LOCATION modules/custom
-  PUPPET_TARGET='custom'  
-fi  
+  printf "\nEnter the location of your custom module (ssh of file path) or nothing if you have none\n> "
+  read custom_location
+
+  if [ -z "$custom_location" ]; then
+    echo "No customization enabled"
+    PUPPET_TARGET='all'  
+  elif [[ $custom_location == ssh://* ]]; then
+    echo "Installing remote custom module..."
+    set -x
+    mkdir -p modules/custom
+    scp -r ${custom_location:6}/* modules/
+    PUPPET_TARGET='custom'  
+  else
+    echo "Installing local custom module..."
+    set -x
+    mkdir -p modules/custom
+    eval EXPANDED_LOCATION=$custom_location/*
+    cp -r $EXPANDED_LOCATION modules/custom
+    PUPPET_TARGET='custom'  
+  fi
+fi
+  
 
 # test for puppet
 dpkg -s puppet >/dev/null
