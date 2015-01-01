7f48f12226810be2fe09f4ee4d90c049f2486b6f
Bruno Bieth <biethb@gmail.com>
2015-01-01 13:11:09 +0100

    do not run oracle accept licence if java8 installer is present

--- modules/java/manifests/java_8.pp
+++ modules/java/manifests/java_8.pp
@@ -13,6 +13,7 @@ class java::java_8 {
   ->
   exec { 'accept-java-license':
     command => '/bin/echo /usr/bin/debconf shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections;/bin/echo /usr/bin/debconf shared/accepted-oracle-license-v1-1 seen true | sudo /usr/bin/debconf-set-selections;',
+    unless  => '/usr/bin/dpkg -s oracle-java8-installer'
   }
   ->
   package { 'oracle-java8-installer' :
