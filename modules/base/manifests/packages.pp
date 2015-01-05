c5c0502eb11dafea53c0805db8f294405167eaba
Bruno Bieth <biethb@gmail.com>
2015-01-05 11:19:29 +0100

    add tree to basic packages

--- modules/base/manifests/packages.pp
+++ modules/base/manifests/packages.pp
@@ -1,6 +1,6 @@
 # provide basic packages
 class base::packages {
-  package { ['htop', 'vim', 'meld', 'curl', 'ack-grep'] : 
+  package { ['htop', 'vim', 'meld', 'curl', 'ack-grep', 'tree'] : 
     ensure => installed,
   }
 }
