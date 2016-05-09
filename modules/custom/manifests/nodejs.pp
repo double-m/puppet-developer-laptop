6ebd67bc931d5e1d54dbfd69d024790ad3a02c35
Marcello Messori <marcello.mm.messori@gmail.com>
2016-05-09 13:04:26 +0200

    nodejs version 5.x -> 6.x; pre-requisite: `sudo rm /etc/apt/sources.list.d/nodesource.list`

--- modules/custom/manifests/nodejs.pp
+++ modules/custom/manifests/nodejs.pp
@@ -1,7 +1,7 @@
 class custom::nodejs {
 	
   exec { "add node source to apt":
-    command => "curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -",
+    command => "curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -",
     onlyif => "test ! -f /etc/apt/sources.list.d/nodesource.list"
   }
   ->
